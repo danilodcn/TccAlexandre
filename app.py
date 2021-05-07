bbae39157cdc1a3016ae4ffa36fcf2babb3b04d1
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-05-07 17:22:56 -0300

    kv funcionando

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from kivy.app import App
 
 from kivy.lang import Builder
 
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file("app.kv")
+
+class HomeScreen(Screen):
+    pass
 
 class TccApp(App):
     title = "Soma de Potencias"
 
     def build(self):
-        return 
+        manager = ScreenManager()
+        manager.add_widget(HomeScreen(name="home"))
+        return manager
+
+if __name__ == "__main__":
+    TccApp().run()
